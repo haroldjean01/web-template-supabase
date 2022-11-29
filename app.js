a7c7a2f28952560721a5a6ba5a0e398f79dc6901
Harold Jean <haroldjean38@yahoo.com>
2022-11-29 14:48:02 -0500

    added submit eventlistener

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
 
-// import { createListItem, deleteList, editListItem, getListItems } from './fetch-utils.js';
+import { createListItem, deleteList, editListItem, getListItems } from './fetch-utils.js';
 // import { renderListItem } from './render-utils.js';
 
 /* Get DOM Elements */
@@ -21,5 +21,12 @@ const error = document.querySelector('#error');
 /* State */
 
 /* Events */
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
 
+    const data = new FormData(form);
+    const item = data.get('item');
+    const rating = data.get('rating');
+    form.reset();
+});
 /* Display Functions */
