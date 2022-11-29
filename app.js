ff86cf650f91d374e2f786141b3ca1e93a6d20b6
Harold Jean <haroldjean38@yahoo.com>
2022-11-29 14:43:40 -0500

    added DOM elements app js

--- app.js
+++ app.js
@@ -2,15 +2,22 @@
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
 
+// import { createListItem, deleteList, editListItem, getListItems } from './fetch-utils.js';
+// import { renderListItem } from './render-utils.js';
+
 /* Get DOM Elements */
-const signInForm = document.getElementById('sign-in');
-const signInEmail = document.getElementById('sign-in-email');
-const signInPassword = document.getElementById('sign-up-password');
+// const signInForm = document.getElementById('sign-in');
+// const signInEmail = document.getElementById('sign-in-email');
+// const signInPassword = document.getElementById('sign-up-password');
 
-const signUpForm = document.getElementById('sign-up');
-const signUpEmail = document.getElementById('sign-up-form');
-const signUpPassword = document.getElementById('sign-up-password');
+// const signUpForm = document.getElementById('sign-up');
+// const signUpEmail = document.getElementById('sign-up-form');
+// const signUpPassword = document.getElementById('sign-up-password');
 
+const form = document.querySelector('.create-form');
+const deleteButton = document.querySelector('#delete-button');
+const listEl = document.querySelector('.list');
+const error = document.querySelector('#error');
 /* State */
 
 /* Events */
