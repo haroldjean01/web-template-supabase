d03818845ec6331c15866308c9cec1af6091f938
Harold Jean <haroldjean38@yahoo.com>
2022-11-30 10:15:12 -0500

    removed 10 from template literal in render file

--- render-utils.js
+++ render-utils.js
@@ -2,7 +2,7 @@ import { getListItems } from './fetch-utils.js';
 
 export function renderListItem(item) {
     const listItemEl = document.createElement('li');
-    listItemEl.textContent = `${item.item}: ${item.quantity}/10`;
+    listItemEl.textContent = `${item.item}: ${item.quantity}`;
 
     return listItemEl;
 }
